c4d42b8380c155924f4e79eefe54061e1924279c
Watkinhj <117215234+Watkinhj@users.noreply.github.com>
2023-12-12 16:28:15 -0500

    Attack Fix

--- FromBelow/Assets/Scripts/Implant Scripts/IndustrialKnifeMovement.cs
+++ FromBelow/Assets/Scripts/Implant Scripts/IndustrialKnifeMovement.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class IndustrialKnifeMovement : PlayerController
 {
     // Update is called once per frame
-
     void Update()
     {
         base.Update();
+
+        // Check for movement input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        FlipSprite(horizontalInput);
     }
 
     protected override void HandleKnifeAttack()
@@ -17,12 +20,29 @@ public class IndustrialKnifeMovement : PlayerController
         {
             knifeObj.SetActive(true);
             Debug.Log("Knife attack!");
-            //animator.SetTrigger("isAttacking");
+            animator.SetBool("isAttacking", true);
         }
         else if (Input.GetMouseButtonUp(1))
         {
             knifeObj.SetActive(false);
             Debug.Log("Knife attack ended.");
+            animator.SetBool("isAttacking", false);
         }
+    }
+
+    // Flip the sprite based on the input direction
+    private void FlipSprite(float horizontalInput)
+    {
+        if (horizontalInput > 0)
+        {
+            // Moving right, flip the sprite to face right
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (horizontalInput < 0)
+        {
+            // Moving left, flip the sprite to face left
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        // If horizontalInput is 0, the player is not moving horizontally, so you may choose to keep the current scale.
     }
 }
--- FromBelow/Assets/Scripts/Implant Scripts/KnifeCollisionDetection.cs
+++ FromBelow/Assets/Scripts/Implant Scripts/KnifeCollisionDetection.cs
@@ -4,23 +4,10 @@ using UnityEngine;
 
 public class KnifeCollisionDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
         {
-            Debug.Log("Enemy hit!");
             Destroy(other.gameObject);
         }
     }
